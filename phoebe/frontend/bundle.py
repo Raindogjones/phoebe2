b661277b7bb0c7cbce58905c5153d1313b774321
Dave <davidjones.pne@gmail.com>
2023-11-15 12:04:29 +0000

    Splitting TMAP into 4 separate atms

--- phoebe/frontend/bundle.py
+++ phoebe/frontend/bundle.py
@@ -3991,7 +3991,7 @@ class Bundle(ParameterSet):
                                             True, 'run_compute')
                         else:
                             atm = self.get_value(qualifier='atm', component=component, compute=compute, context='compute', atm=kwargs.get('atm', None), **_skip_filter_checks)
-                            if atm not in ['ck2004', 'phoenix', 'tmap']:
+                            if atm not in ['ck2004', 'phoenix', 'tmap_sdO', 'tmap_DA', 'tmap_DAO', 'tmap_DO']:
                                 if 'ck2004' in self.get_parameter(qualifier='atm', component=component, compute=compute, context='compute', atm=kwargs.get('atm', None), **_skip_filter_checks).choices:
                                     report.add_item(self,
                                                     "ld_mode='interp' not supported by atm='{}'.  Either change atm@{}@{} or ld_mode@{}@{}.".format(atm, component, compute, component, dataset),
@@ -10313,7 +10313,7 @@ class Bundle(ParameterSet):
                 # all other backends that do not have this parameter, the following
                 # expression will default to 'none'.
                 ld_extrapolation_method = compute_ps.get_value(qualifier='ld_blending_method', component=ldcs_param.component, default='none', **_skip_filter_checks)
-                
+
                 if is_bol:
                     intens_weighting = 'energy'
                 else:
